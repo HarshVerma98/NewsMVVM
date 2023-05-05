62f8243266e2f8f459679e63f8f55057228dba3d
HarshVerma98 <49474526+HarshVerma98@users.noreply.github.com>
2023-05-05 07:31:28 +0530

    Custom Cells Designed

---
+++ NewsAppMVVM/Custom Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsAppMVVM
+//
+//  Created by Harsh Verma on 05/05/23.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    @IBOutlet weak var newsDescriptionLabel: UILabel!
+
+}
